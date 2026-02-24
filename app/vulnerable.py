7f02fdb31f568a3b2acf1626ee541783db23a9c5
Sahilnaik1234 <Sahil.naik@triconinfotech.com>
2026-02-24 12:43:07 +0530

    added vulnurebility

--- app/vulnerable.py
+++ app/vulnerable.py
@@ -31,7 +31,7 @@ def get_user():
     return "User fetched"
 
 # 🚨 Path Traversal
-@app.route("/list")
+@app.route("/lister")
 def read_file():
     filename = request.args.get("file")
     with open(filename, "r") as f:
