ef91dcf25f756b2835dd4989da38b7e24b89dd93
Sahilnaik1234 <Sahil.naik@triconinfotech.com>
2026-02-24 12:51:44 +0530

    added bugs

--- app/vulnerable.py
+++ app/vulnerable.py
@@ -21,7 +21,7 @@ def run():
     return "Executed"
 
 # 🚨 SQL Injection
-@app.route("/user")
+@app.route("/entity")
 def get_user():
     user_id = request.args.get("id")
     conn = sqlite3.connect("users.db")
