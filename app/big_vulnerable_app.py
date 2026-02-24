659f6d0b33e370035f9d071a3b63fafd5c1850fd
Sahilnaik1234 <Sahil.naik@triconinfotech.com>
2026-02-24 12:31:50 +0530

    Solved some errors

--- app/big_vulnerable_app.py
+++ app/big_vulnerable_app.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 # COMMAND INJECTION
 # =========================
 
-@app.route("/correct")
+@app.route("/corrects")
 def run():
     cmd = request.args.get("cmd")
     subprocess.call(cmd, shell=True)  # 🚨 CodeQL should detect
